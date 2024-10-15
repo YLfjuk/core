c9f34c2a842df9cb43ade768960568735b2b9ae7
ylfjuk <ylfjuk@gmail.com>
2024-10-15 10:33:33 +0300

    chore(utils): tests

---
+++ packages/utils/tests/utils/noop.spec.ts
@@ -0,0 +1,41 @@
+import { describe, expect, test } from 'vitest';
+import { noop } from '../../src';
+
+describe('Function that accepts any parameter(s) and returns undefined', () => {
+    test('no params', () => {
+        const actual = noop();
+        expect(actual).toBeUndefined();
+    });
+
+    test('null value', () => {
+        const value = null;
+        const actual = noop(value);
+        expect(actual).toBeUndefined();
+    });
+
+    test('undefined value', () => {
+        const value = undefined;
+        const actual = noop(value);
+        expect(actual).toBeUndefined();
+    });
+
+    test('void value', () => {
+        const value = void 0;
+        const actual = noop(value);
+        expect(actual).toBeUndefined();
+    });
+
+    test('values', () => {
+        const actual = noop(
+            1,
+            2,
+            3,
+            null,
+            [1, 2, 3, undefined],
+            '3252345',
+            new Date()
+        );
+
+        expect(actual).toBeUndefined();
+    });
+});
