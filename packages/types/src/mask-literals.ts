6a9d50b0ff96bad80f3603025e15d4bba76e30ee
ylfjuk <ylfjuk@gmail.com>
2024-12-11 14:41:28 +0200

    chore(types): add jsdoc desctiption for the  type

--- packages/types/src/mask-literals.ts
+++ packages/types/src/mask-literals.ts
@@ -1,6 +1,21 @@
 import type { InverseExtract } from './inverse-extract';
 import type { Primitive } from './primitive';
 
+/**
+ * @description
+ * works the same as
+ * ```ts
+ * type MaskLiterals<T> =
+ *      T extends infer U ?
+ *          U extends number ? number
+ *          : U extends string ? string
+ *              : U extends boolean ? boolean
+ *                  : etc...
+ *      : never
+ * ```
+ *
+ * `K extends boolean ? boolean : K` is used since boolean is often spread into `true | false`
+ */
 export type MaskLiterals<T> = {
     [K in Primitive as `${Primitive}`]: InverseExtract<
         T,
