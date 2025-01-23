a1dd488e4f4ab4ab5a93dcd136882d004dad6bcc
ylfjuk <ylfjuk@gmail.com>
2025-01-23 09:33:20 +0200

    fix(types): Remove duplicated tests

--- packages/types/src/increment.ts
+++ packages/types/src/increment.ts
@@ -7,6 +7,6 @@ import type { IsPositiveOrZero } from './is-positive-or-zero';
  */
 export type Increment<N extends number> = IsPositiveOrZero<N> extends true
     ? [...ArrayOfN<N>, N]['length']
-    : 'error: Unsupported numeric value';
+    : 'Error: Unsupported numeric value';
 
 // TODO: add support for Z, R values
