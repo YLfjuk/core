4af919ab5bc7fc90315cfa1a5bef78a106a7dbe4
ylfjuk <ylfjuk@gmail.com>
2025-01-23 09:47:17 +0200

    feat(types):  type now accepts any type as

---
+++ .changeset/friendly-rocks-care.md
@@ -0,0 +1,5 @@
+---
+"@ylfjuk-core/types": patch
+---
+
+Feat: Modified Types | ArrayOfN | Fill now accepts any type
--- packages/types/src/array-of-n.ts
+++ packages/types/src/array-of-n.ts
@@ -1,18 +1,16 @@
-//? maybe change the order to check if eq before limit (would lead to change in less-than -> less-than-eq-to)
-
 import type { Abs } from './abs';
 
 /**
  * @param Amount - amount of zeros
  * @param Limit - exclusive upper bound
  * @param Acc - A prior accumulation of 0[]
- * @param Fill - A number to fill the array with
+ * @param Fill - What to fill the array with
  */
 export type ArrayOfN<
     Amount extends number,
     Limit extends number = -1,
     Acc extends Fill[] = [],
-    Fill extends number = 0
+    Fill = 0
 > = Abs<Amount> extends Amount
     ? Acc['length'] extends Limit
         ? never
