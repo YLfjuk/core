1fcdea56238c07da42e36f1821f13ba63f12e1e1
ylfjuk <ylfjuk@gmail.com>
2025-03-10 10:35:42 +0200

    feat(types): Split the Suggest type into Suggest & SuggestStr, Add a Whatever type

---
+++ packages/types/src/whatever.ts
@@ -0,0 +1,15 @@
+import type { Maybe } from './maybe';
+import type { Suggest } from './suggest';
+
+/**
+ * @description
+ * Suggests the provided type {@link T}, but allows for any value
+ *
+ * @see {@link Suggest}
+ * @see {@link Maybe}
+ * @see {@link https://x.com/colinhacks/status/1891069007299522877?s=19}
+ *
+ * @alternative
+ * type Whatever<T> = T | {} | null | undefined;
+ */
+export type Whatever<T> = Maybe<Suggest<T>>;
