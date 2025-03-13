28911cc163e468071684f750584c40fda8d85de4
ylfjuk <ylfjuk@gmail.com>
2025-03-13 18:38:34 +0200

    .

--- playground/types/src/pick-optionals.ts
+++ playground/types/src/pick-optionals.ts
@@ -1,9 +1,9 @@
 'use new';
 
 // TODO: Try to preserve the original type (`| undefined` or no `| undefined`) | works with `exactOptionalPropertyTypes` set to true
-export type PickOptionals<T> = Partial<{
-    [K in keyof T as T[K] extends Required<T>[K] ? never : K]-?: T[K];
-}>;
+export type PickOptionals<T> = {
+    [K in keyof T as T[K] extends Required<T>[K] ? never : K]: T[K];
+};
 
 // region Tests
 type Test = PickOptionals<{ bob: number; bert?: string }>;
