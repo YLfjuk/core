d6714748ccc0806e1c0fe7aff194c668835d9968
ylfjuk <ylfjuk@gmail.com>
2025-03-28 23:06:38 +0300

    .

--- playground/types/src/concat.ts
+++ playground/types/src/concat.ts
@@ -1,11 +1,17 @@
-import type { UnknownArray } from '@ylfjuk-core/types';
+type Settings = {
+    element: unknown;
+};
 
-export type Concat<ArrA extends UnknownArray, ArrB extends UnknownArray> = [
-    ...ArrA,
-    ...ArrB
-];
+export type Concat<
+    ArrA extends readonly Options['element'][],
+    ArrB extends readonly Options['element'][],
+    Options extends Settings = Settings
+> = [...ArrA, ...ArrB];
 
 // #region Tests
 
 type Test = Concat<[1, 2, 3], ['a', 'b', 'c']>;
+//   ^?
+
+type Test2 = Concat<[1, 2, 3], [3, 2, 1], { element: number }>;
 //   ^?
