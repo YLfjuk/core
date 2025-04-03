17511168a0f4a771e26747a36c9f6dc450d235e7
ylfjuk <ylfjuk@gmail.com>
2025-04-03 14:55:03 +0300

    v0.0.14

--- playground/types/src/date-to-str.ts
+++
@@ -1,8 +0,0 @@
-import type { Replace } from './replace';
-
-export type DateToStr<T> = Replace<T, Date, string>;
-
-// region Tests
-
-type Test2_4 = DateToStr<{ bob: Date; bert: { berta: Date } }>;
-//   ^?
