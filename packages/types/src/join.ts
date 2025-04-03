aaea9431ba2d4be7698b0eb148bddd6d316977f6
ylfjuk <ylfjuk@gmail.com>
2025-04-03 18:13:08 +0300

    fix(types): Updated import

--- packages/types/src/join.ts
+++ packages/types/src/join.ts
@@ -1,5 +1,5 @@
 import type { Stringifiable } from './stringifiable';
-import type { Stringify } from './stringify';
+import type { ToStr } from './to-str';
 
 type Settings = { separator: string };
 type DefaultSettings = { separator: '' };
@@ -19,7 +19,7 @@ export type Join<
     infer S extends Stringifiable,
     ...infer Rest extends Stringifiable[]
 ]
-    ? `${Stringify<S>}${Join<Rest, Options> extends infer U extends string
+    ? `${ToStr<S>}${Join<Rest, Options> extends infer U extends string
           ? U extends ''
               ? ''
               : `${Options['separator']}${U}`
