c54d0063e6118a51bb7b600be2eb46d7ed700824
ylfjuk <ylfjuk@gmail.com>
2025-06-13 11:45:46 +0300

    dump

--- playground/utils/src/bool.ts
+++ playground/utils/src/bool.ts
@@ -1,3 +1,3 @@
-import { Falsy } from '../../types/src/falsy';
+import type { Falsy } from '../../types/src/falsy';
 
 export const bool = <T>(value: T): value is Exclude<T, Falsy> => Boolean(value);
