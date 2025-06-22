1eebb4a7be78239e7ed94ab130ed25e495f7a0c2
ylfjuk <ylfjuk@gmail.com>
2025-06-22 10:13:31 +0300

    fix(utils): fix false assumption

--- packages/utils/src/json/stringify.ts
+++ packages/utils/src/json/stringify.ts
@@ -1,5 +1,5 @@
 import type { FN, Stringifiable, ToStr } from "@ylfjuk-core/types";
-import type { ReplacerFN } from "./types/helpers";
+import type { ReplacerFN, StringifiablePrimitive } from "./types/helpers";
 
 type JsonStringify = {
 	/**
@@ -20,18 +20,18 @@ type JsonStringify = {
 		value: undefined | symbol | FN,
 		replacer?: ReplacerFN,
 		space?: string | number,
-	): undefined;
+	): string | undefined;
 
 	/**
 	 * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
 	 * @param value A JavaScript value, usually an object or array, to be converted.
-	 * @param replacer A function that transforms the results.
-	 * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+	 * @param replacer Is not used in this case, so it is set to `never`.
+	 * @param spaceIs Is not used in this case, so it is set to `never`.
 	 */
-	<T extends Stringifiable>(
+	<T extends StringifiablePrimitive | string>(
 		value: T,
-		replacer?: ReplacerFN,
-		space?: string | number,
+		replacer?: never,
+		space?: never,
 	): ToStr<T>;
 
 	/**
@@ -40,7 +40,31 @@ type JsonStringify = {
 	 * @param replacer A function that transforms the results.
 	 * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
 	 */
-	(value: unknown, replacer?: ReplacerFN, space?: string | number): string;
+	<T extends StringifiablePrimitive | string>(
+		value: T,
+		replacer?: ReplacerFN,
+		space?: string | number,
+	): ToStr<T> | undefined;
+
+	/**
+	 * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+	 * @param value A JavaScript value, usually an object or array, to be converted.
+	 * @param replacer Is not used in this case, so it is set to `never`.
+	 * @param spaceIs Is not used in this case, so it is set to `never`.
+	 */
+	(value: object, replacer?: never, space?: never): string;
+
+	/**
+	 * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+	 * @param value A JavaScript value, usually an object or array, to be converted.
+	 * @param replacer A function that transforms the results.
+	 * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+	 */
+	(
+		value: object,
+		replacer?: ReplacerFN,
+		space?: string | number,
+	): string | undefined;
 
 	/**
 	 * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
@@ -72,11 +96,23 @@ type JsonStringify = {
 	 * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
 	 * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
 	 */
-	<T extends Stringifiable>(
+	<T extends StringifiablePrimitive | string>(
 		value: T,
 		replacer?: (number | string)[] | null,
 		space?: string | number,
 	): ToStr<T>;
+
+	/**
+	 * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+	 * @param value A JavaScript value, usually an object or array, to be converted.
+	 * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
+	 * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+	 */
+	(
+		value: object,
+		replacer?: (number | string)[] | null,
+		space?: string | number,
+	): string;
 
 	/**
 	 * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
